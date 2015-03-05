f53767419a2c60174038aeac134896a9cb3e5222
Gabriel <gabrielg.desouza@gmail.com>
2015-03-05 01:36:07 -0800

    Basic Layout of CombatScene
    This is the basic layout of the game. It have based in a 864x486
    resolution, but it can change during the development.

---
+++ Assets/Scripts/GameSetting.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSetting{
+
+
+	static private GameSetting instance;
+	
+	public const string TITLE = "TITLE - RESOURCE SIMULATION";
+
+
+	/// <summary>
+	/// Singleton instance of GameSettigs.
+	/// </summary>
+	/// <value>The instance.</value>
+	static public GameSetting Instance 
+	{
+		get{
+			if (instance == null)
+				instance = new GameSetting();
+
+			return instance;
+		}
+	}
+
+	//making constructor private, to be only instance of GameSetting
+	private GameSetting()
+	{
+
+	}
+}
